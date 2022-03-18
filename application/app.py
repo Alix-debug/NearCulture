e861046e3572d50781ec9453e013dd3e6d41542c
Francoise <francoise.ruch@edu.devinci.fr>
2022-03-18 18:46:23 +0100

    update: traveler"

--- application/app.py
+++ application/app.py
@@ -34,9 +34,42 @@ def Museum():
 	data_museum = response.json()
 	result_query=""
 	for i in range(0, len(data_museum['results']["bindings"])) :
-		result_query += data_museum['results']["bindings"][0]["name"]["value"] + 
+		result_query += data_museum['results']["bindings"][0]["name"]["value"] 
 	#results
 	return render_template("index.html",content = response.json())
+
+@app.route("/Travelers")
+def Traveler():
+	query = """
+	
+		prefix :      <http://www.semanticweb.org/protegee> 
+		prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+		prefix owl:   <http://www.w3.org/2002/07/owl#> 
+		prefix xml:   <http://www.w3.org/XML/1998/namespace> 
+		prefix xsd:   <http://www.w3.org/2001/XMLSchema#> 
+		prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> 
+	
+	SELECT  ?age ?birthDate ?familyName ?givenName ?latitude ?longitude WHERE {
+			?Instance 
+			rdf:type owl:NamedIndividual  ; :age ?age; 	
+	                             	:birthDate ?birthDate; 
+	                             	:familyName ?familyName; 
+									:givenName ?givenName;
+	        						:latitude ?latitude;
+									:longitude ?longitude;
+									
+	} 
+
+	"""
+	header = {'Content-type': 'application/sparql-query'}
+	response = requests.post('http://localhost:3030/Travelers/', data=query, headers = header)
+	data_traveler = response.json()
+	result_query=""
+	for i in range(0, len(data_traveler['results']["bindings"])) :
+		result_query += data_traveler['results']["bindings"] 
+	#results
+	return render_template("index.html",content = response.json())
+
 
 @app.route("/queries")
 def query():
